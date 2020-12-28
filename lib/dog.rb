7a0ae0ac9b83f01031d0b3f7db11b540f130c205
Jordan Reed <jordanreed03@gmail.com>
2020-12-28 19:46:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,2 @@
 # Add your code here
 
-require 'pry'
-
-class Dog 
-  @@all = [ ]
-
-  def initialize(name)
-    @name = name
-    @@all << self 
-  end 
-
-  def self.all
-    @@all
-  end 
-
-  def self.clear_all
-    @@all.clear 
-  end 
-  
-  def self.print_all
-    @@all.each do |x|
-      puts x.name
-    end
-  end
-end 
